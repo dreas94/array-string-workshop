09c102434388d91f694660e88cddad6fbcd14e9d
Andreas Eriksson <dreas94@gmail.com>
2022-03-31 10:41:32 +0200

    Tested and Completed Part 2

--- src/main/java/se/lexicon/dreas94/App.java
+++ src/main/java/se/lexicon/dreas94/App.java
@@ -8,18 +8,32 @@ import java.util.Arrays;
  */
 public class App 
 {
+    private static NameRepository nameRepository = new NameRepository();
+
     public static void main( String[] args )
     {
-        NameRepository nameRepository = new NameRepository();
-        String[] names = new String[] {"Erik", " ", "Svensson"};
+        String[] names = new String[] {"Erik Svensson", "Andreas Eriksson", "Mehrdad Javan"};
         nameRepository.setNames(names);
-        printAboutNameRepository(nameRepository);
+        printAboutNameRepository();
         nameRepository.clear();
-        printAboutNameRepository(nameRepository);
+        printAboutNameRepository();
+
+        addNameToNameRepository("Leo Yoosefi");
+        addNameToNameRepository("Leo Yoosefi");
+        addNameToNameRepository("Pär Törnell");
+        addNameToNameRepository("Simon Elbrink");
+
+        printAboutNameRepository();
     }
 
-    private static void printAboutNameRepository(NameRepository nameRepository)
+    private static void printAboutNameRepository()
     {
         System.out.println("The Array has " + nameRepository.getSize() + " Strings and contains the following: " + Arrays.toString(nameRepository.findAll()));
+    }
+
+    private static void addNameToNameRepository(final String fullName)
+    {
+        if(!nameRepository.add(fullName))
+            System.out.println(fullName + " is already in the repository!");
     }
 }
--- src/main/java/se/lexicon/dreas94/NameRepository.java
+++ src/main/java/se/lexicon/dreas94/NameRepository.java
@@ -6,6 +6,11 @@ public class NameRepository
 {
     private static String[] names = new String[]{};
 
+    private static void sortNames()
+    {
+        Arrays.sort(NameRepository.names);
+    }
+
     public static int getSize()
     {
         return names.length;
@@ -14,6 +19,7 @@ public class NameRepository
     public static void setNames(String[] names)
     {
         NameRepository.names = Arrays.copyOf(names,names.length);
+        sortNames();
     }
 
     public static void clear()
@@ -24,5 +30,24 @@ public class NameRepository
     public static String[] findAll()
     {
         return names;
+    }
+
+    public static String find(final String fullName)
+    {
+        if(getSize() == 0 || Arrays.binarySearch(names,fullName) < 0)
+            return fullName;
+
+        return null;
+    }
+
+    public static boolean add(final String fullName)
+    {
+        if(find(fullName) == null)
+            return false;
+
+        names = Arrays.copyOf(names, names.length + 1);
+        names[names.length - 1] = fullName;
+
+        return true;
     }
 }
